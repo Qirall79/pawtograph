b33a8c8df81b9385f89c23ba74df7c13d0e1929b
qirall79 <belfatmi79@gmail.com>
2023-09-05 14:02:53 +0100

    Add follow, adopt, lost, conversations, notifications urls to authenticated-users-only urls

--- middleware.js
+++ middleware.js
@@ -1,5 +1,17 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/", "/profile", "/profile/:path*", "/settings"],
+  matcher: [
+    "/",
+    "/profile",
+    "/profile/:path*",
+    "/settings",
+    "/conversations",
+    "/conversations/:path*",
+    "/notifications",
+    "/adopt",
+    "/lost",
+    "/following",
+    "/followers",
+  ],
 };
