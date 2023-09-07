26682056302ede7f0d3e723f0ab35647a09303b3
qirall79 <belfatmi79@gmail.com>
2023-09-07 15:07:30 +0100

    Revalidate path on search

--- app/api/users/search/route.ts
+++ app/api/users/search/route.ts
@@ -1,11 +1,11 @@
 import prismadb from "@/lib/prismadb";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: Request) => {
   try {
-    console.log(req);
     const users = await prismadb.user.findMany({});
-
+    revalidatePath(req.url);
     return NextResponse.json({ users }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
